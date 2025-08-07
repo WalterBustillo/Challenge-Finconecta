925a3b81288ed0afc84a79dc452233c17efd99ca
WalterBustillo <91577811+WalterBustillo@users.noreply.github.com>
2025-08-07 07:55:29 -0400

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 from loguru import logger
+from utils.logging_config import logger
 
 def transform_data(data: pd.DataFrame) -> pd.DataFrame:
     """
